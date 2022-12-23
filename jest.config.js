938d3eb7eeabe969dd3ce96db92c8709615cf49d
Xhamps <xhamps@gmail.com>
2022-12-23 18:21:48 -0300

    Config test Jest

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+  clearMocks: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  coverageReporters: ['lcov', 'html', 'text'],
+  modulePathIgnorePatterns: ['dist', 'node_modules', 'coverage'],
+  coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: [ '/node_modules/']
+};
